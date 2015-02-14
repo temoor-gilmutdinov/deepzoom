21a8da96641fc2d400e6a74acec6b4d3677f023b
Temoor Gilmutdinov <temoor.gilmutdinov@propeople.com.ua>
2015-02-14 16:58:21 +0200

    Memory processors, additional configs, own table, cleanup after file deletion

--- engines/deepzoom.openseadragon.js
+++ engines/deepzoom.openseadragon.js
@@ -1,12 +1,14 @@
-(function($) {
+(function ($) {
   Drupal.behaviors.deepzoom = {
-    attach: function(context) {
-      $('.deepzoom').each(function() {
+    attach: function (context) {
+      $('.deepzoom', context).each(function () {
         id = $(this).attr('id').split('-');
         viewer = new OpenSeadragon.Viewer({
           id: id.join('-'),
           prefixUrl: Drupal.settings.deepZoom.library + '/images/',
           tileSources: Drupal.settings.deepZoom.path + '/' + id[1] + '.dzi',
+          minZoomImageRatio: Drupal.settings.deepZoom.min_zoom_ratio,
+          maxZoomPixelRatio: Drupal.settings.deepZoom.max_zoom_ratio
         });
       });
     }
